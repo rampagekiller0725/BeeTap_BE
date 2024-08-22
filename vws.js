7f50a309af7796fdc28e389e3defb3aeba751770
rampagekiller0725 <rampagekiller0725@gmail.com>
2024-08-22 04:22:46 -0400

    mine levels

--- vws.js
+++ vws.js
@@ -415,6 +415,7 @@ app.post('/purchase-mine-card', async (req, res) => {
       });
     }
 
+    console.log(user, user['mine_levels']);
     console.log(user.mine_levels, user.balance);
     user.mine_levels[index]++;
     user.balance -= purchase_amount;
